1f67653f301071efc9a02f61173afc55fe357c9c
Nahin <mnk1@andrew.cmu.edu>
2020-07-07 09:10:10 +0300

    developed text box and synchronous chat and channel creation features

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 import os
 import random
+from bisect import bisect
 
 from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO, emit
@@ -11,44 +12,61 @@ socketio = SocketIO(app)
 unique_id_counter = 0
 server_side_memory = {'unique_id_counter': 0}
 
+max_no_of_messages = 100
+
+channels = ["food", 'programming', 'parenting', 'tigers', 'travel', 'politics', 'gaming', 'cars', 'work', 'cooking',
+            'history']
+messages = {}
+for each_channel in channels:
+    messages[each_channel] = []
+
+channels = sorted(list(messages.keys()), key=str.lower)
+
 
 @app.route("/")
 def index(**kwargs):
-    default_options = {"logged_in": False}
-    for k, v in kwargs.items():
-        default_options[k] = v
-    return render_template('index.html', **default_options)
+    return render_template('index.html')
 
 
 @app.route("/get-messages", methods=["POST"])
 def get_messages():
-    print("blabla")
-    channels = ["disease", 'red', 'zoos', 'tigers', 'food', 'travel', 'politics', 'Bellamy', 'contrive', 'Jill',
-                'McKeon', 'voice', 'pilot', 'Elizabeth', 'paste', 'broom', 'Malaysia', 'abode', 'laconic', 'imbecile',
-                'desiderata', 'quintessential', 'pray', 'ternary', 'lamprey', 'beep', 'mulct', 'directorate',
-                'baptistery', 'jersey', 'Oberlin', 'clever', 'silicic', 'DC', 'pad', 'incommensurable', 'mutiny']
-
-    messages = {}
     unique_id = request.form.get("unique_id")
-    print(unique_id, 'uniqe id')
-    for channel in channels:
-        random_list = []
-        selection_options = [{'sender': 'paul', 'unique_id': '0', 'message': 'Hey, what is up!', 'timestamp': '3:20pm'},
-                             {'sender': 'jake', 'unique_id': '2', 'message': 'Nothing much man what about you paul?',
-                              'timestamp': '4:20pm'},
-                             {'sender': 'Nahin', 'unique_id': '1', 'message': 'Hey guys good to see ya!',
-                              'timestamp': '5:20pm'}]
-        for i in range(100):
-            message_to_append = random.choice(selection_options)
-            message_to_append['is_myself'] = message_to_append['unique_id'] == unique_id
+    channel_requested = request.form.get("channel")
+    list_of_messages = messages[channel_requested]
+    for message in list_of_messages:
+        message['is_myself'] = int(message['unique_id']) == int(unique_id)
+    return jsonify(list_of_messages)
 
-            random_list.append(message_to_append)
-        messages[channel] = random_list
-    return jsonify(messages)
+
+@app.route("/get-channels", methods=["POST"])
+def get_channels():
+    return jsonify(channels)
 
 
 @app.route("/get-unique-id", methods=["POST"])
 def get_unique_id():
     server_side_memory['unique_id_counter'] += 1
     return jsonify({'unique_id': server_side_memory['unique_id_counter']})
+
+
+@socketio.on("new message")
+def new_message_broadcast(data):
+    data['timestamp'] = data['timestamp'].replace(' ', '').lower()
+    channel = data['channel']
+    new_message = {k: v for k, v in data.items() if k != "channel"}
+    messages[channel].append(new_message)
+    messages[channel] = messages[channel][-max_no_of_messages:]
+    data['max_no_of_messages'] = max_no_of_messages
+    emit("message update", data, broadcast=True)
+
+
+@socketio.on("new channel")
+def new_channel(data):
+    new_channel_name = data['new_channel_name']
+    assert(new_channel_name not in messages)
+    messages[new_channel_name] = []
+    insertion_point = bisect(channels, new_channel_name)
+    channels.insert(insertion_point, new_channel_name)
+    data['insert_after'] = channels[insertion_point - 1] if insertion_point > 0 else -1
+    emit("new channel created", data, broadcast=True)
 
--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // localStorage.clear();
-    // Functions to load pages
+
     document.querySelector('body').classList.add('enter_out');
 
-    // document.querySelector('body').classList.add('enter_out');
+
     const logged_out = Handlebars.compile(document.querySelector('#logged_out').innerHTML);
     const logged_in = Handlebars.compile(document.querySelector('#logged_in').innerHTML);
-
+    Handlebars.registerPartial('each_channel_partial', document.querySelector('#each_channel_partial_template').innerHTML);
     function load_page(selector_reference, is_first_load, ...args) {
         let x,y,z;
         if (is_first_load) {
@@ -69,64 +68,160 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }
 
-    let channels;
+    let active_channel;
     let messages;
 
-    function load_logged_in_page(is_first_load) {
+    function channel_name_onclick(channel_name_div) {
+        active_channel = channel_name_div.dataset.channel_name;
+        localStorage.setItem('active_channel', active_channel);
+        update_messages(active_channel);
+        document.querySelectorAll('.channel-name').forEach(channel_name_div => {
+            channel_name_div.classList.remove('selected');
+        });
+        channel_name_div.classList.add('selected');
+        return false;
 
+    }
+    function load_logged_in_page(is_first_load) {
+        // const channels = get_channels();
+        get_channels( channels => {
+        const delay = load_page(logged_in, is_first_load, {'display_name': localStorage.getItem('display_name')});
+        setTimeout( () => {
+            channels_loader(channels);
+            if (localStorage.getItem('active_channel')) {
+                active_channel = localStorage.getItem('active_channel');
+            }
+            else if (channels.length > 0){
+                active_channel = channels[0]
+            }
+
+            update_messages(active_channel);
+
+            document.querySelectorAll('.channel-name').forEach(channel_name_div => {
+                channel_name_div.onclick = () => channel_name_onclick(channel_name_div);
+                if (channel_name_div.dataset.channel_name===active_channel) channel_name_div.onclick();
+            });
+
+            // Connect to websocket
+            const socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+            // When connected, configure buttons
+            socket.on('connect', () => {
+
+                // Each button should emit a "submit vote" event
+                document.querySelector('#message-send-form').onsubmit = () => {
+                        const message_text = document.querySelector('#message-input-area').value;
+                        if (message_text.length > 0) {
+                            const dateWithoutSecond = new Date();
+                            const time_string = dateWithoutSecond.toLocaleTimeString([], {hour: 'numeric', minute:'2-digit'});
+                            socket.emit('new message', {'unique_id': localStorage.getItem('unique_id'),
+                                'sender': localStorage.getItem('display_name'),
+                                'message': message_text, 'timestamp': time_string, 'channel': active_channel});
+                            document.querySelector('#message-input-area').value = '';
+                        }
+                        return false;
+                    };
+
+                document.querySelector('#new-channel-form').onsubmit = () => {
+                    const new_channel_name = document.querySelector('#new-channel-input').value;
+                    document.querySelector('#new-channel-input').value = '';
+                    if (new_channel_name.length > 0) {
+                        get_channels(current_channels => {
+                            if (current_channels.indexOf(new_channel_name) > -1) {
+                                //In the array!
+                                document.querySelector(`[data-channel_name='${new_channel_name}']`).onclick()
+                            } else {
+                                //Not in the array
+                                socket.emit('new channel', {'new_channel_name': new_channel_name})
+                            }
+
+                        });
+
+
+                    }
+
+                    return false;
+                };
+                });
+
+
+            socket.on('message update', data => {
+                if (data['channel'] === active_channel) {
+                    const {channel, ...message} = data;
+                    message['is_myself'] = parseInt(message['unique_id']) === parseInt(localStorage.getItem('unique_id'));
+                    messages.push(message);
+                    messages = messages.slice(-data['max_no_of_messages'], messages.length);
+                    messages_loader(active_channel);
+                }
+
+
+            });
+
+            socket.on('new channel created', data => {
+                const new_channel_name = data['new_channel_name'];
+                const one_channel_div_generator = Handlebars.compile(document.querySelector('#each_channel_partial_template').innerHTML);
+                const one_channel_html = one_channel_div_generator({'channel_name': new_channel_name});
+                const where_to_insert_after = data['insert_after'];
+                if (where_to_insert_after === -1) {
+                    //will come first
+                    document.querySelector('#channels-template-insertion-zone').insertAdjacentHTML('afterbegin', one_channel_html)
+                }
+                else {
+                    document.querySelector(`[data-channel_name=${where_to_insert_after}`).insertAdjacentHTML('afterend', one_channel_html);
+                }
+                const channel_name_div = document.querySelector(`[data-channel_name=${new_channel_name}`);
+                channel_name_div.onclick = () => channel_name_onclick(channel_name_div);
+                document.querySelector(`[data-channel_name='${new_channel_name}']`).onclick();
+            });
+
+
+        }, delay);
+
+    });
+    }
+
+    function get_channels( next_action) {
+        const request = new XMLHttpRequest();
+        request.open('POST', '/get-channels');
+        request.onload = () => {
+            const channels = JSON.parse(request.responseText);
+            next_action(channels);
+        };
+        request.send()
+    }
+
+    function update_messages(channel_selection) {
         const request = new XMLHttpRequest();
         request.open('POST', '/get-messages');
         // Callback function for when request completes
         request.onload = () => {
-            // Extract JSON data from request
-            const data = JSON.parse(request.responseText);
-            console.log(data);
-            // Update the result div
-            messages = data;
-            channels = Object.keys(messages);
-
-            const active_channel = 'Malaysia';
-            const delay = load_page(logged_in, is_first_load, {'display_name': 'Nahin', 'channels': channels});
-            setTimeout( () => {
-
-                update_messages(active_channel);
-
-                document.querySelectorAll('.channel-name').forEach(channel_name_div => {
-                    channel_name_div.onclick = () => {
-                        const selection = channel_name_div.dataset.channel_name;
-                        update_messages(selection);
-                        document.querySelectorAll('.channel-name').forEach(channel_name_div =>
-                                                            {channel_name_div.classList.remove('selected');});
-                        channel_name_div.classList.add('selected');
-                        return false;
-
-                    };
-                    if (channel_name_div.dataset.channel_name===active_channel) channel_name_div.onclick();
-                });
-
-
-            }, delay);
-
-            };
-
+            messages = JSON.parse(request.responseText);
+            messages_loader(channel_selection);
+        };
         // Add data to send with request
         const data = new FormData();
         data.append('unique_id', localStorage.getItem('unique_id'));
+        data.append('channel', channel_selection);
 
         // Send request
         request.send(data);
 
-
     }
 
-    function update_messages(channel_selection) {
-        //only call when logged in page is loaded!
+    function messages_loader(channel_selection) {
         const messages_template_generator = Handlebars.compile(document.querySelector('#messages_template').innerHTML);
         document.querySelector('#message-template-insertion-zone').innerHTML =
-            messages_template_generator({'messages': messages[channel_selection]});
+            messages_template_generator({'messages': messages, 'channel_name': channel_selection});
+        const objDiv = document.querySelector('div.messages-subsection');
+        objDiv.scrollTo({top: objDiv.scrollHeight, behavior: 'smooth'});
 
     }
 
+    function channels_loader(channels) {
+        const channels_template_generator = Handlebars.compile(document.querySelector('#channels_template').innerHTML);
+        document.querySelector('#channels-template-insertion-zone').innerHTML =
+            channels_template_generator({'channels': channels});
+    }
 
     // Check if display name was previously selected by this user. Then load the correct page.
     if (!localStorage.getItem('display_name') || !localStorage.getItem('unique_id')) {
@@ -138,13 +233,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     }
-
-
-
-
-
-
-
 
 
 
